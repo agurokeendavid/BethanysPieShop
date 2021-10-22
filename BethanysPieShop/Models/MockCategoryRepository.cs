3f7c8b2624751bd4cbd99f3d25d48130d5a1149a
Keen David Aguro <agurokeendavid@gmail.com>
2021-10-22 10:00:05 +0800

    Created models and repository.

---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => new List<Category>
+        {
+            new() { CategoryId = 1, CategoryName = "Fruit Pies", Description = "All-fruity pies" },
+            new() {CategoryId = 2, CategoryName = "Cheese cakes", Description = "Cheesy all the way"},
+            new() {CategoryId = 3, CategoryName = "Seasonal pies", Description = "Get in the mood for a seasonal pie"}
+        };
+    }
+}
